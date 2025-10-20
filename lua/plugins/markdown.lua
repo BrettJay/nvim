bee3f8707e02e31cdac58f777e148c32f089a7a8
Brett Jones <brett.jones@sourcegraph.com>
2025-10-20 08:23:08 +0800

    Remove

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = function(_, opts)
+      opts.formatters_by_ft = opts.formatters_by_ft or {}
+      opts.formatters_by_ft.markdown = {}
+    end,
+  },
+  {
+    "mfussenegger/nvim-lint",
+    opts = function(_, opts)
+      opts.linters_by_ft = opts.linters_by_ft or {}
+      opts.linters_by_ft.markdown = {}
+    end,
+  },
+}
