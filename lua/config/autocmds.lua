3ac4f8cf0f7850a2bf38794248c69231b10a13c7
Brett Jones <brett@ampcode.com>
2026-02-02 09:37:41 +0800

    update

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -24,3 +24,12 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.spell = true
   end,
 })
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function()
+    local save_cursor = vim.fn.getpos(".")
+    vim.cmd([[%s/\s\+$//e]])
+    vim.fn.setpos(".", save_cursor)
+  end,
+})
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -19,5 +19,52 @@ return {
       end,
       desc = "Toggle terminal",
     },
+    {
+      "<leader>gc",
+      function()
+        Snacks.terminal("zsh -ic 'pnpm fix'", {
+          cwd = vim.fn.getcwd(),
+          win = { position = "bottom", height = 0.3 },
+          interactive = true,
+        })
+      end,
+      desc = "Run check",
+    },
+    {
+      "<leader>ge",
+      function()
+        Snacks.picker.git_log({
+          confirm = function(picker, item)
+            if not item then
+              return
+            end
+
+            local commit = item.commit or item.hash or item.oid
+            if not commit then
+              return
+            end
+
+            picker:close()
+
+            vim.schedule(function()
+              local files = {}
+              for _, f in ipairs(vim.fn.systemlist("git show --name-only --pretty=format: " .. commit)) do
+                if f ~= "" then
+                  table.insert(files, { text = f, file = f, pos = { 1, 0 } })
+                end
+              end
+
+              Snacks.picker.pick({
+                items = files,
+                format = "file",
+                title = "Files: " .. commit:sub(1, 7),
+                confirm = "jump",
+              })
+            end)
+          end,
+        })
+      end,
+      desc = "Git: Explore commits → files",
+    },
   },
 }
